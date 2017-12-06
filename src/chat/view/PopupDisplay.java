eb210f61af6373b811bb4053fb1f36aeab1d91ee
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-12-06 08:26:17 -0700

    added code

--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,6 +1,11 @@
 package chat.view;
 
+import javax.swing.JOptionPane;
+
 public class PopupDisplay
 {
-
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
 }
